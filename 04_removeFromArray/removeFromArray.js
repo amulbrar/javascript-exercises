0d46f48ddea3f984cae456445b7bfabd32fcef2c
Amul <amulbrar24@gmail.com>
2024-06-10 15:36:49 +0530

    passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...toBeRemoved) {
+    let i = 0;
+    for(let h = 0; h < toBeRemoved.length; h++){
+        for (i = 0; i < array.length; i++) {
+            if (array[i] ===  toBeRemoved[h]) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
