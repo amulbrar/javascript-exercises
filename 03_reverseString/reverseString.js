77fee83db1deb8cf49c3da8bd1d43d43f7f672e7
Amul <amulbrar24@gmail.com>
2024-06-10 13:51:01 +0530

    passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let aux_array = string.split("");
+    let output = "";
+    
+    for(let i = (aux_array.length - 1); i >= 0; i--) {
+        output += `${aux_array[i]}`; 
+    }
+    return output;
 };
 
 // Do not edit below this line
